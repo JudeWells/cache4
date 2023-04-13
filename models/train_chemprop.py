2c2a3a4e4f82a5ef65a36840b7d0b6324a2f1778
Jude Wells <judewells@hotmail.com>
2023-04-13 23:54:11 +0100

    script for training chemprop

---
+++ models/train_chemprop.py
@@ -0,0 +1,17 @@
+import chemprop
+
+arguments = [
+    '--data_path', 'ligand_files/chemprop_data.csv',
+    '--dataset_type', 'classification',
+    '--save_dir', 'chemprop_model',
+    '--split_type', 'scaffold_balanced',
+    '--target_columns', 'target',
+    '--class_balance',
+    '--ensemble_size', '8',
+]
+
+if __name__ == '__main__':
+    args = chemprop.args.TrainArgs().parse_args(arguments)
+    mean_score, std_score = chemprop.train.cross_validate(args=args, train_func=chemprop.train.run_training)
+    print(mean_score)
+    print(std_score)
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,3 +1,6 @@
+from utils.scaffold_split import make_decoy_df, get_scaffold
+import pandas as pd
+
 def get_label(d):
     if d=="101-1000":
         return(550.5)
@@ -16,4 +19,20 @@ def get_label(d):
     if d=="3001-10000":
         return(6500.5)
     if d==">10000":
-        return(10000) 
+        return(10000)
+
+
+def make_joined_dataset(hit_path="ligand_files/CBLB_inhibitors.csv",
+                        decoy_path="ligand_files/decoys/found_decoys.sdf", save_path=None):
+    decoys = make_decoy_df(decoy_path)
+    decoys["ic_50_nM"] = 20000
+    hits = pd.read_csv(hit_path)
+    hits["ic_50_nM"] = hits["IC50_range_nM"].apply(get_label)
+    hits["target"] = 1
+    hits["scaffold"] = hits["smiles"].apply(get_scaffold)
+    df = pd.concat([hits, decoys])
+    if save_path is not None:
+        df.to_csv(save_path, index=False)
+    return df
+
+bp=1

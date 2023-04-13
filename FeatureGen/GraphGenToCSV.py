73a71dd18f070e267b159450b98bd981f50513d5
ferdiekrammer <fgpk20@bath.ac.uk>
2023-04-13 22:31:49 +0100

    data loader

--- FeatureGen/GraphGenToCSV.py
+++ FeatureGen/GraphGenToCSV.py
@@ -5,18 +5,14 @@ import pandas as pd
 import os
 from pathlib import Path
 
-#%%
-
 filePath=Path()
 print(filePath)
-#%%
 dataPath=filePath/'../ligand_files/CBLB_inhibitors.csv'
 print(os.path.exists(dataPath))
 
 df=pd.read_csv(dataPath)
 df.info()
-# %%
-# GraphDF=pd.DataFrame(columns=['node features','bonds','bond features','target value'])
+
 # %%
 df=df.drop(columns=['sdf_file_ix','COMPOUND_NAMES','Reference'])
 df.info()
@@ -25,25 +21,19 @@ df.head
 df['Target']=df['IC50_range_nM'].apply(get_label)
 df.head()
 # %%
-df['node features','bonds','bond features']=df['smiles'].apply(GraphInfoGen)
-df.head()
+# df['smiles'].apply(GraphInfoGen)
+# %%
+df['tempstor']=df['smiles'].apply(GraphInfoGen)
+df.info()
+# %%
+print(len(df['tempstor'][1]))
 
-# # %%
-# for row_smile, row_target in zip(df['smiles'],df['IC50_range_nM']):
-#     GraphRow=[]
-#     nodeFeatures,bond,bond_f=GraphInfoGen(row_smile)
-#     GraphRow.append(nodeFeatures)
-#     GraphRow.append(bond)
-#     GraphRow.append(bond_f)
-#     GraphRow.append(get_label(row_target))
-#     print(GraphRow)
-#     GraphDF.loc[len(df.index)]=GraphRow
-# # %%
-# GraphDF.info()
-# # %%
-# GraphDF.to_csv('GraphDataSet.csv')
-# # df['node features','bonds','bond features']=df.map)
-# # %%
+
+# %%
+df['node features']=df['tempstor']
+# %%
+df['bonds']=df['tempstor'][1]
+df['bond features']=df['tempstor'][2]
 
 # %%
 df.to_csv('GraphDataSet.csv')

8bfd0e93fd4c81d1ca9fd17f76724f4f2904e9a1
ferdiekrammer <fgpk20@bath.ac.uk>
2023-04-13 11:47:23 +0100

    atom and bond Feature Gen update

--- FeatureGen/GraphGen.py
+++ FeatureGen/GraphGen.py
@@ -1,3 +1,7 @@
-""" This note book contains functions which will generate the graph from a smiles string """
+""" 
+
+This note book contains functions which will generate the graph from a smiles string 
+
+
+"""
 # In[]
-import rdkit
